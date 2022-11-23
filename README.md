412cc270970af6fc658ab399e14e4c4214979122
haxxe123 <1188402+haxxe123@users.noreply.github.com>
2022-11-23 22:46:16 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">SOLID Princple</h1>
+
+## Purpose of this repository
+
+This is only a place to store basic code showing SOLID principle.
---
+++ solid-principle/solid-principle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace solid_principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
